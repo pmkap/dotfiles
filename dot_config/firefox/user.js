481c3cb067c2a97f5d664b6d8094a2ace84fc2ef
Peter Kaplan <peter@pkap.de>
2025-01-30 10:00:29 +0100

    firefox user.js and mimeapps.list

---
+++ dot_config/firefox/user.js
@@ -0,0 +1,9 @@
+// touchpad strange scrolling behaviour
+user_pref("apz.gtk.kinetic_scroll.enabled", false);
+
+// Touchpad gestures
+user_pref("browser.gesture.swipe.left", "");
+user_pref("browser.gesture.swipe.right", "");
+
+// better experience tabbing through search results
+user_pref("browser.urlbar.resultMenu.keyboardAccessible", false);
